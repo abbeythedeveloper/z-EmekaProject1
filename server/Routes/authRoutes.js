ec91591dfb53c69508912d5149f1ca33e34b4c8f
Abbeythedeveloper <ogundiranabbey057@gmail.com>
2023-10-07 00:39:29 -0700

    s

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -15,7 +15,7 @@ router.use(
 router.get('/', test)
 router.post('/register',registerUser)
 router.post('/login',loginUser)
-router.get('/profile',getProfile)
+router.get(getProfile)
 
 router.get('/login/success', (req, res) => {
     if (req.user) {
